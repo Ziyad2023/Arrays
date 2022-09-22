03ec346ea5f2baa093a12205511bcf3a36bb80e2
Ziyad Wakil <ziyad.wakil@mrjaffesclass.com>
2022-09-22 11:44:53 -0700

    Day 2

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -4,6 +4,13 @@ public class Arrays
     public static void main(String[] args) 
     {
         String[] array ={"Alpha","Bravo","Charlie","Delta","Echo"};
-        for (int i = 0; i < myArray.length; i++)
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        for (String value : array) {
+            System.out.print(value);
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(length);
+        }
     }
-}
