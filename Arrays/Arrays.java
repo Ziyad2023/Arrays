76a623b44669691357405ac414a16a7c16ce47e3
Ziyad Wakil <ziyad.wakil@mrjaffesclass.com>
2022-09-23 11:44:21 -0700

    last part

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -4,13 +4,14 @@ public class Arrays
     public static void main(String[] args) 
     {
         String[] array ={"Alpha","Bravo","Charlie","Delta","Echo"};
+        int part2 = array.length -1;
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            System.out.println("array[i]");
         }
         for (String value : array) {
             System.out.print(value);
         }
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(length);
+        for (int h = 0; h < array.length; h++) {
+            System.out.print(array[h]);
         }
     }
